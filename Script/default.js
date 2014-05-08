09b8332ccfa1012f14e2431d513800d938b752f9
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-05-08 09:33:31 -0700

    Map now resizes when browser is resized.

--- Script/default.js
+++ Script/default.js
@@ -3,9 +3,33 @@ require(["esri/map", "dojo/domReady!"], function (Map) {
 	"use strict";
 
 	var map;
+
+	/** Set the height of the map div.
+*/
+	function setMapDivHeight() {
+		var topNavBar, mapDiv, desiredHeight;
+
+		topNavBar = document.getElementById("topNavBar");
+		mapDiv = document.getElementById("map");
+
+		desiredHeight = window.innerHeight - topNavBar.clientHeight - 40;
+
+		mapDiv.style.height = [desiredHeight, "px"].join("");
+
+		//if (map) {
+		//	map.resize();
+		//}
+	}
+
+	setMapDivHeight();
+
+	window.addEventListener("resize", setMapDivHeight, true);
+	window.addEventListener("deviceorientation", setMapDivHeight, true);
+
 	map = new Map("map", {
-		center: [-56.049, 38.485],
-		zoom: 3,
-		basemap: "streets"
+		basemap: "hybrid",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
 	});
 });

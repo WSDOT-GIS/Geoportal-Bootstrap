e1fb5dd6fab7bdae088b91b72639979796e51451
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-05-07 17:27:51 -0700

    Created project

---
+++ Script/default.js
@@ -0,0 +1,11 @@
+﻿/*global require*/
+require(["esri/map", "dojo/domReady!"], function (Map) {
+	"use strict";
+
+	var map;
+	map = new Map("map", {
+		center: [-56.049, 38.485],
+		zoom: 3,
+		basemap: "streets"
+	});
+});
